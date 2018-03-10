ae03ba7029e2dff3e161dd8910c639cbff130e9a
KrystianRK <krystian.czekalski@resourcekraft.com>
2018-03-10 19:37:59 +0000

    with allowed hosts

--- movierater/settings.py
+++ movierater/settings.py
@@ -23,6 +23,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '10.0.2.2',
     'movie-rater-full.herokuapp.com'
 ]
 
@@ -61,6 +63,7 @@ CORS_ORIGIN_WHITELIST = (
     'localhost:8000',
     '127.0.0.1:9000',
     'localhost:4200',
+    'localhost:49152',
     '10.0.2.2',
     'movie-rater-web.firebaseapp.com'
 )
